4591dae75034165c91e2749426e5d85310090343
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-10-30 11:33:19 -0300

    up

---
+++ Sprint4/Gufos/src/pages/profile.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      // My styles
+    },
+  });
+  
+  export default styles;
+
+class Profile extends Component{
+    
+
+    
+    render(){
+        return< Text>Nome Estático</Text>
+
+
+    }
+}
+
+export default Profile;
